ee1de67c311da2919b23b2b7bebc4d47c880813c
Darren Rambaud <xyzst@users.noreply.github.com>
2016-11-20 01:30:36 -0600

    ~1/3 complete with addVehicle() method

--- src/dealership/DealershipGUI.java
+++ src/dealership/DealershipGUI.java
@@ -1,15 +1,28 @@
 package dealership;
 
+import layout.SpringUtilities;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.List;
 import java.awt.event.*;
 import java.io.*;
 import java.util.*;
 
+
 public class DealershipGUI extends JFrame {
     private static Dealership dummy = Dealership.readDatabase(),
                               db = (dummy == null) ? new Dealership() : dummy;
+
+    private final static String PASSENGERPANEL = "PASSENGER VEHICLE",
+                                MOTORCYCLEPANEL = "MOTORCYCLE",
+                                TRUCKPANEL = "TRUCK";
+
+    private final static int extraWindowWidth = 100;
+
+    DealershipGUI() {
+
+    }
 
     private DealershipGUI (String title) {
         super(title);
@@ -58,6 +71,12 @@ public class DealershipGUI extends JFrame {
         addvehicle.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ev) {
                 field.setText("STATUS: in adding vehicle ...");
+                Thread qThread = new Thread() {
+                    public void run() {
+                        addVehicle();
+                    }
+                };
+                qThread.start();
             }
         });
 
@@ -145,7 +164,7 @@ public class DealershipGUI extends JFrame {
         String[] header = {"TYPE", "VIN", "MAKE", "MODEL", "YEAR", "MILEAGE", "PRICE", "BODY STYLE", "TYPE",
                             "DISPLACEMENT (cc)", "LENGTH (ft)", "MAX LOAD WEIGHT (lb)"};
         String body_style, type, displ, load, length;
-        Object[][] data = new Object[db.getVehicleInvSize()][12];
+        Object[][] data = new Object[db.getVehicleInvSize()][header.length];
 
         for (int i = 0; i < db.getVehicleInvSize(); ++i) {
             if(db.getVehicleAtPosition(i) instanceof PassengerCar) {
@@ -200,14 +219,155 @@ public class DealershipGUI extends JFrame {
         display.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
     }
 
+    public void addVehicle() {
+        JFrame frame = new JFrame("Adding vehicle");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        //Create and set up the content pane.
+        DealershipGUI x = new DealershipGUI();
+        x.newPanelComponent(frame.getContentPane());
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
     public void terminateSession() {
         db.writeDatabase();
         System.exit(0);
     }
 
-//    public void showDialog(Component f) {
-//        JOptionPane.showMessageDialog(f, "Session has been successfully saved!");
-//    }
+    public void newPanelComponent(Container pane) {
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        // Panel for adding a new passenger vehicle
+        JPanel card1 = new JPanel(new GridLayout(7,1,1,1)) {
+            public Dimension getPreferredSize() {
+                Dimension size = super.getPreferredSize();
+                size.width += extraWindowWidth;
+                return size;
+            }
+        };
+        JLabel pcVIN = new JLabel("VIN (string)");
+        JTextField pcVINTF = new JTextField("", 12);
+        card1.add(pcVIN);
+        card1.add(pcVINTF);
+        JLabel pcMAKE = new JLabel("Make (string)");
+        JTextField pcMAKETF = new JTextField("", 12);
+        card1.add(pcMAKE);
+        card1.add(pcMAKETF);
+        JLabel pcMODEL = new JLabel("Model (string)");
+        JTextField pcMODELTF = new JTextField("", 12);
+        card1.add(pcMODEL);
+        card1.add(pcMODELTF);
+        JLabel pcYEAR = new JLabel("Year (int)");
+        JTextField pcYEARTF = new JTextField("", 12);
+        card1.add(pcYEAR);
+        card1.add(pcYEARTF);
+        JLabel pcMILEAGE = new JLabel("Mileage (int)");
+        JTextField pcMILEAGETF = new JTextField("", 12);
+        card1.add(pcMILEAGE);
+        card1.add(pcMILEAGETF);
+        JLabel pcPRICE = new JLabel("Price (float)");
+        JTextField pcPRICETF = new JTextField("", 12);
+        card1.add(pcPRICE);
+        card1.add(pcPRICETF);
+        JLabel pcBODY = new JLabel("Body Style (string)");
+        JTextField pcBODYTF = new JTextField("", 12);
+        card1.add(pcBODY);
+        card1.add(pcBODYTF);
+        JButton pcSUBMIT = new JButton ("Submit");
+        card1.add(pcSUBMIT);
+        JButton pcCLEAR = new JButton("Clear");
+        card1.add(pcCLEAR);
+
+        pcSUBMIT.addActionListener(new ActionListener() {
+            public void actionPerformed (ActionEvent e) {
+                Thread qThread = new Thread() {
+                    public void run() {
+                        ArrayList<String> err = new ArrayList<>();
+                        if (pcVINTF.getText().length() == 0) {
+                            err.add("Field 'VIN' is empty");
+                        }
+                        else if (pcVINTF.getText().length() > 10) {
+                            err.add("'VIN' length is too long!");
+                        }
+
+                        if (pcMAKETF.getText().length() == 0) {
+                            err.add("Field 'Make' is empty");
+                        }
+
+                        if (pcMODELTF.getText().length() == 0) {
+                            err.add("Field 'Model' is empty");
+                        }
+
+                        if (pcYEARTF.getText().length() != 4) {
+                            err.add("'Year' value is invalid");
+                        }
+                        else if (Integer.parseInt(pcYEARTF.getText()) < 0) {
+                            err.add("'Year' cannot be a negative value");
+                        }
+                        else if (Integer.parseInt(pcYEARTF.getText()) < 1886 ||
+                                Integer.parseInt(pcYEARTF.getText()) > 2020) {
+                            err.add("'Year'  is not within the acceptable range");
+                        }
+
+                        if (pcMILEAGETF.getText().length() == 0) {
+                            err.add("Field 'Mileage' is empty");
+                        }
+                        else if (Integer.parseInt(pcMILEAGETF.getText()) < 0) {
+                            err.add("'Mileage' cannot be a negative value");
+                        }
+
+                        if (pcPRICETF.getText().length() == 0) {
+                            err.add("Field 'Price' is empty");
+                        }
+                        else if (Float.parseFloat(pcPRICETF.getText()) < 0) {
+                            err.add("'Price' cannot be a negative value");
+                        }
+
+                        if (pcBODYTF.getText().length() == 0) {
+                            err.add("Field 'Body Style' is empty");
+                        }
+
+                        if (err.isEmpty()) {
+                            // add vehicle to FIXME stopped here
+                        }
+                        else {
+                            // show dialog message with errors FIXME stopped here
+                        }
+                    }
+                };
+                qThread.start();
+            }
+        });
+
+        pcCLEAR.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                pcVINTF.setText("");
+                pcMAKETF.setText("");
+                pcYEARTF.setText("");
+                pcMILEAGETF.setText("");
+                pcPRICETF.setText("");
+                pcBODYTF.setText("");
+            }
+        });
+
+        // Panel for adding a new motorcycle
+        JPanel card2 = new JPanel();
+
+        card2.add(new JTextField("TextField", 20));
+
+        // Panel for adding a new truck
+        JPanel card3 = new JPanel();
+        card3.add(new JTextField("TextField", 20));
+
+        tabbedPane.addTab(PASSENGERPANEL, card1);
+        tabbedPane.addTab(MOTORCYCLEPANEL, card2);
+        tabbedPane.addTab(TRUCKPANEL, card3);
+
+        pane.add(tabbedPane, BorderLayout.WEST);
+    }
 
     public static void main (String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
